5abeda57432fbe9d69691085d92eb7d37fb127ed
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-05-07 18:58:37 -0400

    Tags belongToMany Products through ProductTag

--- models/index.js
+++ models/index.js
@@ -18,6 +18,10 @@ Product.belongsToMany(Tag, {
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
