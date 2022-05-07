a4d27f2dcd33ba0aca88247c31e71f8304a5832f
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-05-07 18:56:01 -0400

    Categories have many Products

--- models/index.js
+++ models/index.js
@@ -5,11 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Products.belongsTo(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
