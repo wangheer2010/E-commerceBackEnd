8affc019419157856e6537f915ab24922ce0c59a
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-05-07 20:13:54 -0400

    Update function

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,8 +49,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const CategoryData = await Category.update(req.body,{
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
