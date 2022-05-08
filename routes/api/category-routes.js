cf19547d5096d71ac90b75eb650c76e7abdd8004
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-05-07 20:12:04 -0400

    Delete method

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,16 +37,40 @@ router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const locationData = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const CategoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
