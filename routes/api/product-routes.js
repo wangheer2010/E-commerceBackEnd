72bd93786764e18b73c16e81f0ae68ed05670476
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-05-07 21:56:23 -0400

    Delete product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,8 +111,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const ProductData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!ProductData) {
+      res.status(404).json({ message: 'No Product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(ProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
