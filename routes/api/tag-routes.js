48853d4061dd2810b0fd111fa57aacd89819d3c8
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-05-07 22:08:31 -0400

    Delete tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -66,8 +66,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const TagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!TagData) {
+      res.status(404).json({ message: 'No Tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(TagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
