005da63fc5494cfddbc9806919084bd02ae4cedf
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-05-07 22:07:24 -0400

    Update tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,8 +47,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const TagData = await Tag.update(req.body,{
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!TagData) {
+      res.status(404).json({ message: 'No Tag found with that id!' });
+      return;
+    }
+    res.status(200).json(TagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
